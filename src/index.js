0411472619eb45c91cd790312b0ec9ba9875b802
unemployedpangolin015 <115036973+unemployedpangolin015@users.noreply.github.com>
2022-12-07 19:09:51 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from '@chakra-ui/react';
+import { HashRouter } from 'react-router-dom';
+
+import theme from './theme';
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/800.css'
+import '@fontsource/raleway/400.css'
+import '@fontsource/raleway/700.css'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/700.css'
+import '@fontsource/work-sans/800.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
